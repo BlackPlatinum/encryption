ba82690b87367ba4a34631f35aa0a5d06d114543
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-10-16 02:44:08 +0330

    AES class added

---
+++ PHPGuard/AES/AES.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author Baha2r
+ * @license MIT
+ * Date: 16/Oct/2019
+ *
+ * Main AES encryption class to encrypt and decrypt data
+ **/
+
+namespace PHPGuard\AES;
+
+
+class AES
+{
+
+}
