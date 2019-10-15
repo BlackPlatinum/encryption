0174a21e0da96edf6ab474c41cd47d43438ccc2d
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-10-16 02:39:30 +0330

    base interfaces added

---
+++ PHPGuard/Core/Decryption.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author Baha2r
+ * @license MIT
+ * Date: 16/Oct/2019
+ *
+ * Base decryption interface
+ **/
+
+namespace PHPGuard\Core;
+
+
+interface Decryption
+{
+    public function decryption();
+}
---
+++ PHPGuard/Core/Encryption.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author Baha2r
+ * @license MIT
+ * Date: 16/Oct/2019
+ *
+ * Base encryption interface
+**/
+
+namespace PHPGuard\Core;
+
+
+interface Encryption
+{
+    public function encryption();
+}
