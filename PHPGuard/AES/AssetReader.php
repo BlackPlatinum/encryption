2718cbaa8f6a0db8e890b66dd4953af39844afe3
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-10-17 22:16:18 +0330

    AssetReader class added

---
+++ PHPGuard/AES/AssetReader.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @author Baha2r
+ * @license MIT
+ * Date: 16/Oct/2019
+ *
+ * Main Reader class to read encryption key and initial vector from file
+ **/
+
+namespace PHPGuard\AES;
+
+
+use PHPGuard\Core\Reader;
+
+class AssetReader implements Reader
+{
+    public function readKey()
+    {
+        // TODO: Implement readKey() method.
+    }
+
+    public function readIV()
+    {
+        // TODO: Implement readIV() method.
+    }
+
+}
