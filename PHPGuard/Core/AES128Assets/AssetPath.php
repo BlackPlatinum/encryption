97006583bbd637bc3292e1ac7cca701c2369c265
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-10-18 02:20:15 +0330

    AssetPath class added

---
+++ PHPGuard/Core/AES128Assets/AssetPath.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @author Baha2r
+ * @license MIT
+ * Date: 18/Oct/2019
+ *
+ * This class find absolute path of assets directory
+ **/
+
+namespace PHPGuard\Core\AES128Assets;
+
+
+abstract class AssetPath
+{
+
+    /**
+     * Find find absolute path of assets directory
+     * @return string returns absolute path of assets directory
+     */
+    public static function getAbsolutePath()
+    {
+        return __DIR__;
+    }
+}
