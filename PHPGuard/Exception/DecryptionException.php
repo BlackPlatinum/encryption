8713a33628d5a524f78341740de5168aa4241b0f
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-10-18 17:19:00 +0330

    exception classes created

---
+++ PHPGuard/Exception/DecryptionException.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author Baha2r
+ * @license MIT
+ * Date: 18/Oct/2019
+ *
+ * Main decryption exception class
+ **/
+
+namespace PHPGuard\Exception;
+
+use RuntimeException;
+
+class DecryptionException extends RuntimeException
+{
+}
---
+++ PHPGuard/Exception/EncryptionException.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author Baha2r
+ * @license MIT
+ * Date: 18/Oct/2019
+ *
+ * Main encryption exception class
+ **/
+
+namespace PHPGuard\Exception;
+
+use RuntimeException;
+
+class EncryptionException extends RuntimeException
+{
+}
