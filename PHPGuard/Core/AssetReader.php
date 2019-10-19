9881d449b1c6d7f10c09ff1bcffbb65baf4caabe
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-10-19 18:58:44 +0330

    code summarized, security of key improved

--- PHPGuard/Core/AssetReader.php
+++ PHPGuard/Core/AssetReader.php
@@ -21,6 +21,7 @@ abstract class AssetReader extends AssetPath
      * Decodes binary values to unsigned chars
      * @param array $bytes Input byte array
      * @return string returns decoded byte array $bytes
+     * @deprecated
      */
     private function compute($bytes)
     {
@@ -34,7 +35,34 @@ abstract class AssetReader extends AssetPath
 
 
     /**
-     * Converts binary file key to unsigned chars
+     * Converts each element of an array to a character and concat them all to a binary or normal string
+     * @param array $array the input array
+     * @return string return a binary or normal string
+     */
+    private function arrayToString($array)
+    {
+        return implode(array_map("chr", $array));
+    }
+
+
+    /**
+     * Reads content of a binary file
+     * @param string $path path of the binary file
+     * @return string return content of binary file
+     */
+    private function reader($path)
+    {
+        $size = filesize($path);
+        $handle = fopen($path, "rb");
+        $binary = fread($handle, $size);
+        fclose($handle);
+        $barr = unpack("c*", $binary);
+        return $this->arrayToString($barr);
+    }
+
+
+    /**
+     * Converts binary file key to binary string
      * @return string returns key value
      * @throws RuntimeException throws exception if key file is not found
      */
@@ -43,17 +71,12 @@ abstract class AssetReader extends AssetPath
         $path = $this->getAbsolutePath() . "/AES128Assets/k";
         if (!is_file($path) || !file_exists($path))
             throw new RuntimeException("File not found!");
-        $size = filesize($path);
-        $handle = fopen($path, "rb");
-        $binary = fread($handle, $size);
-        fclose($handle);
-        $barr = unpack("c*", $binary);
-        return $this->compute($barr);
+        return $this->reader($path);
     }
 
 
     /**
-     * Convert binary file initial vector to unsigned chars
+     * Convert binary file initial vector to binary string
      * @return string returns initial vector value
      * @throws RuntimeException throws exception if initial vector file is not found
      */
@@ -62,17 +85,12 @@ abstract class AssetReader extends AssetPath
         $path = $this->getAbsolutePath() . "/AES128Assets/iv";
         if (!is_file($path) || !file_exists($path))
             throw new RuntimeException("File not found!");
-        $size = filesize($path);
-        $handle = fopen($path, "rb");
-        $binary = fread($handle, $size);
-        fclose($handle);
-        $barr = unpack("c*", $binary);
-        return $this->compute($barr);
+        return $this->reader($path);
     }
 
 
     /**
-     * Converts binary file key to unsigned chars
+     * Converts binary file key to binary string
      * @return string returns key value
      * @throws RuntimeException throws exception if key file is not found
      */
@@ -81,17 +99,12 @@ abstract class AssetReader extends AssetPath
         $path = $this->getAbsolutePath() . "/AES256Assets/k";
         if (!is_file($path) || !file_exists($path))
             throw new RuntimeException("File not found!");
-        $size = filesize($path);
-        $handle = fopen($path, "rb");
-        $binary = fread($handle, $size);
-        fclose($handle);
-        $barr = unpack("c*", $binary);
-        return $this->compute($barr);
+        return $this->reader($path);
     }
 
 
     /**
-     * Convert binary file initial vector to unsigned chars
+     * Convert binary file initial vector to binary string
      * @return string returns initial vector value
      * @throws RuntimeException throws exception if initial vector file is not found
      */
@@ -100,11 +113,6 @@ abstract class AssetReader extends AssetPath
         $path = $this->getAbsolutePath() . "/AES256Assets/iv";
         if (!is_file($path) || !file_exists($path))
             throw new RuntimeException("File not found!");
-        $size = filesize($path);
-        $handle = fopen($path, "rb");
-        $binary = fread($handle, $size);
-        fclose($handle);
-        $barr = unpack("c*", $binary);
-        return $this->compute($barr);
+        return $this->reader($path);
     }
 }
