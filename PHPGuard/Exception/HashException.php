bedf35d222a3488cbe2a1a9965cf4d552fa0b0fa
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-10-19 21:09:35 +0330

    hash exception handler class created

---
+++ PHPGuard/Exception/HashException.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author Baha2r
+ * @license MIT
+ * Date: 18/Oct/2019
+ *
+ * Main hash exception class
+ **/
+
+namespace PHPGuard\Exception;
+
+use RuntimeException;
+
+class HashException extends RuntimeException
+{
+}
