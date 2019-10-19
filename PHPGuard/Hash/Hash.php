1b54838dfceb0e0c1d929214d22764ae5c77823e
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-10-19 20:56:01 +0330

    Hash class created

---
+++ PHPGuard/Hash/Hash.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author Baha2r
+ * @license MIT
+ * Date: 16/Oct/2019
+ *
+ * Main hash maker class to hash all type of data
+ **/
+
+namespace PHPGuard\Hash;
+
+
+abstract class Hash
+{
+
+}
