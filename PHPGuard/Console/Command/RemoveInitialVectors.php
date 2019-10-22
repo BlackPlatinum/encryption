75fd5381336950c2498313bf09aea4c512f56be7
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-10-23 00:20:24 +0330

    These command class completed

--- PHPGuard/Console/Command/RemoveInitialVectors.php
+++ PHPGuard/Console/Command/RemoveInitialVectors.php
@@ -4,7 +4,36 @@
 namespace PHPGuard\Console\Command;
 
 
-class RemoveInitialVectors
-{
+use PHPGuard\Console\Commands;
+use RuntimeException;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
+class RemoveInitialVectors extends Commands
+{
+    protected static $defaultName = "rm:iv";
+
+    public function __construct(string $name = null)
+    {
+        parent::__construct($name);
+    }
+
+
+    protected function configure()
+    {
+        $this->setDescription("Removes initial vector files in related directories.")
+            ->setHelp("<comment>\nThis command allows you to remove initial vector files.\nYou can generate new initial vectors with: php guard gen:iv\nYou maybe need to change to user root if you got permission denied error.\n</comment>");
+        parent::configure();
+    }
+
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln("");
+        $ans = parent::removeIVs();
+        if (!$ans)
+            throw new RuntimeException("Permission denied!");
+        $output->writeln("<info>Initial vectors removed successfully</info>");
+        $output->writeln("");
+    }
 }
--- PHPGuard/Console/Command/RemoveKeys.php
+++ PHPGuard/Console/Command/RemoveKeys.php
@@ -4,7 +4,37 @@
 namespace PHPGuard\Console\Command;
 
 
-class RemoveKeys
+use PHPGuard\Console\Commands;
+use RuntimeException;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RemoveKeys extends Commands
 {
 
+    protected static $defaultName = "rm:key";
+
+    public function __construct(string $name = null)
+    {
+        parent::__construct($name);
+    }
+
+
+    protected function configure()
+    {
+        $this->setDescription("Removes key files in related directories.")
+            ->setHelp("<comment>\nThis command allows you to remove key files.\nYou can generate new keys with: php guard gen:key\nYou maybe need to change to user root if you got permission denied error.\n</comment>");
+        parent::configure();
+    }
+
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln("");
+        $ans = parent::removeKeys();
+        if (!$ans)
+            throw new RuntimeException("Permission denied!");
+        $output->writeln("<info>Keys removed successfully</info>");
+        $output->writeln("");
+    }
 }
