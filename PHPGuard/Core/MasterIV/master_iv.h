dcc349fd5370debaa80f573f478322be885fdeb4
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-10-31 00:43:53 +0330

    Master iv files added

---
+++ PHPGuard/Core/MasterIV/master_iv.h
@@ -0,0 +1,15 @@
+/**
+ * @author  Baha2r
+ * @license MIT
+ * Date: 31/Oct/2019 00:31 AM
+ *
+ * Master IV generator header
+ **/
+
+#ifndef PHPGuard_MASTER_IV_H
+#define PHPGuard_MASTER_IV_H
+
+#endif //PHPGuard_MASTER_IV_H
+
+#include <bits/stdc++.h>
+#include <openssl/rand.h>
