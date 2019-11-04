c6352f0fcdabe432f164df1c1436fba29db32b45
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-11-05 02:18:59 +0330

    Kernel class added

---
+++ PHPGuard/Console/Core/Kernel.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @author  Baha2r
+ * @license MIT
+ * Date: 22/Oct/2019 23:28 PM
+ *
+ * Kernel of commands represents in this class
+ **/
+
+namespace PHPGuard\Console\Core;
+
+
+use PHPGuard\Crypto\Algorithms\AES;
+use PHPGuard\Hash\Hash;
+use PHPGuard\Core\AssetPath;
+use Symfony\Component\Console\Exception\RuntimeException;
+use Symfony\Component\Console\Command\Command;
+
+class Kernel extends Command
+{
+    protected function SetAdminKey()
+    {
+
+    }
+}
