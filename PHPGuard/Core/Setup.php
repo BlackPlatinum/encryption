e55ead7d621426599da3c26e52fb04be60008fd1
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-11-05 03:16:42 +0330

    Setup class completed

--- PHPGuard/Core/Setup.php
+++ PHPGuard/Core/Setup.php
@@ -5,13 +5,48 @@
  * @license MIT
  * Date: 05/Nov/2019 02:22 AM
  *
- * Setups key and initial vector for each registered user
+ * Setup key and initial vector for each registered user
  **/
 
 namespace PHPGuard\Core;
 
 
-class Setup
+use PHPGuard\Hash\Hash;
+
+abstract class Setup
 {
 
+    /**
+     * Constructor
+     */
+    protected function __construct()
+    {
+        //
+    }
+
+
+    /**
+     * Setup a key for each user
+     *
+     * @param  string  $key  Entered key
+     *
+     * @return string return the constructed key
+     */
+    protected function setupKey($key)
+    {
+        return Hash::sha512([":  ژ~'".$key."` ز- /|  "], true);
+    }
+
+
+    /**
+     * Setup an initial vector for each user
+     *
+     * @param  string  $iv  Entered iv
+     *
+     * @return string return the constructed initial vector
+     */
+    protected function setupIV($iv)
+    {
+        return substr(Hash::sha384(["Q& ^".$iv.";?پ ="], true), 31, 16);
+    }
 }
