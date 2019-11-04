d67bb6609cd3c67bcdda39f7aba06b648a8b2885
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-11-05 02:23:23 +0330

    Setup class added

---
+++ PHPGuard/Core/Setup.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author  Baha2r
+ * @license MIT
+ * Date: 05/Nov/2019 02:22 AM
+ *
+ * Setups key and initial vector for each registered user
+ **/
+
+namespace PHPGuard\Core;
+
+
+class Setup
+{
+
+}
