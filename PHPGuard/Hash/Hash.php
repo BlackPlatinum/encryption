7ec19315fd9a6760c1ce69edb59717adb780b96b
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-11-05 03:52:23 +0330

    security methods improved

--- PHPGuard/Hash/Hash.php
+++ PHPGuard/Hash/Hash.php
@@ -39,18 +39,20 @@ abstract class Hash implements BaseHash
     /**
      * Make digested hash from a value by a specific hash algorithm
      *
-     * @param  string|mixed  $data       the data is being hashed
-     * @param  string        $method     the hash algorithm
-     * @param  bool          $serialize  [optional] if $serialize is true, $data can be any type but resources
+     * @param  string|mixed  $data        The data is being hashed
+     * @param  string        $method      The hash algorithm
+     * @param  bool          $serialize   [optional] if $serialize is true, $data can be any type but resources
+     *
+     * @param  bool          $raw_output  [optional] Setting to true will return as raw output data, otherwise the return
+     *                                    value is binhex encoded
      *
      * @return false|string return the digested hash value on success or false on failure
-     * @throws HashException throws exception if could not hash the data
      */
-    private static function hash($data, $method, $serialize = false)
+    private static function hash($data, $method, $serialize = false, $raw_output = false)
     {
         $digest = null;
         if ($serialize === true) {
-            $digest = openssl_digest(json_encode(serialize($data)), $method);
+            $digest = openssl_digest(json_encode(serialize($data)), $method, $raw_output);
             if ($digest === false) {
                 throw new HashException("Could not hash the data!");
             }
@@ -70,95 +72,113 @@ abstract class Hash implements BaseHash
     /**
      * Make digested hash from a value by this specific hash algorithm
      *
-     * @param  string|mixed  $data       the data is being hashed
-     * @param  bool          $serialize  [optional] if $serialize is true, $data can be any type but resources.
-     *                                   it uses json encode to convert any types to string
+     * @param  string|mixed  $data        the data is being hashed
+     * @param  bool          $serialize   [optional] if $serialize is true, $data can be any type but resources.
+     *                                    it uses json encode to convert any types to string
+     *
+     * @param  bool          $raw_output  [optional] Setting to true will return as raw output data, otherwise the return
+     *                                    value is binhex encoded
      *
      * @return false|string return the digested hash value on success or false on failure
      * @throws HashException throws exception if could not hash the data
      */
-    public static function md5($data, $serialize = false)
+    public static function md5($data, $serialize = false, $raw_output = false)
     {
-        return self::hash($data, self::MD5, $serialize);
+        return self::hash($data, self::MD5, $serialize, $raw_output);
     }
 
 
     /**
      * Make digested hash from a value by this specific hash algorithm
      *
-     * @param  string|mixed  $data       the data is being hashed
-     * @param  bool          $serialize  [optional] if $serialize is true, $data can be any type but resources.
-     *                                   it uses json encode to convert any types to string
+     * @param  string|mixed  $data        the data is being hashed
+     * @param  bool          $serialize   [optional] if $serialize is true, $data can be any type but resources.
+     *                                    it uses json encode to convert any types to string
+     *
+     * @param  bool          $raw_output  [optional] Setting to true will return as raw output data, otherwise the return
+     *                                    value is binhex encoded
      *
      * @return false|string return the digested hash value on success or false on failure
      * @throws HashException throws exception if could not hash the data
      */
-    public static function sha1($data, $serialize = false)
+    public static function sha1($data, $serialize = false, $raw_output = false)
     {
-        return self::hash($data, self::SHA1, $serialize);
+        return self::hash($data, self::SHA1, $serialize, $raw_output);
     }
 
 
     /**
      * Make digested hash from a value by this specific hash algorithm
      *
-     * @param  string|mixed  $data       the data is being hashed
-     * @param  bool          $serialize  [optional] if $serialize is true, $data can be any type but resources.
-     *                                   it uses json encode to convert any types to string
+     * @param  string|mixed  $data        the data is being hashed
+     * @param  bool          $serialize   [optional] if $serialize is true, $data can be any type but resources.
+     *                                    it uses json encode to convert any types to string
+     *
+     * @param  bool          $raw_output  [optional] Setting to true will return as raw output data, otherwise the return
+     *                                    value is binhex encoded
      *
      * @return false|string return the digested hash value on success or false on failure
      * @throws HashException throws exception if could not hash the data
      */
-    public static function sha224($data, $serialize = false)
+    public static function sha224($data, $serialize = false, $raw_output = false)
     {
-        return self::hash($data, self::SHA224, $serialize);
+        return self::hash($data, self::SHA224, $serialize, $raw_output);
     }
 
 
     /**
      * Make digested hash from a value by this specific hash algorithm
      *
-     * @param  string|mixed  $data       the data is being hashed
-     * @param  bool          $serialize  [optional] if $serialize is true, $data can be any type but resources.
-     *                                   it uses json encode to convert any types to string
+     * @param  string|mixed  $data        the data is being hashed
+     * @param  bool          $serialize   [optional] if $serialize is true, $data can be any type but resources.
+     *                                    it uses json encode to convert any types to string
+     *
+     * @param  bool          $raw_output  [optional] Setting to true will return as raw output data, otherwise the return
+     *                                    value is binhex encoded
      *
      * @return false|string return the digested hash value on success or false on failure
      * @throws HashException throws exception if could not hash the data
      */
-    public static function sha256($data, $serialize = false)
+    public static function sha256($data, $serialize = false, $raw_output = false)
     {
-        return self::hash($data, self::SHA256, $serialize);
+        return self::hash($data, self::SHA256, $serialize, $raw_output);
     }
 
 
     /**
      * Make digested hash from a value by this specific hash algorithm
      *
-     * @param  string|mixed  $data       the data is being hashed
-     * @param  bool          $serialize  [optional] if $serialize is true, $data can be any type but resources.
-     *                                   it uses json encode to convert any types to string
+     * @param  string|mixed  $data        the data is being hashed
+     * @param  bool          $serialize   [optional] if $serialize is true, $data can be any type but resources.
+     *                                    it uses json encode to convert any types to string
+     *
+     * @param  bool          $raw_output  [optional] Setting to true will return as raw output data, otherwise the return
+     *                                    value is binhex encoded
      *
      * @return false|string return the digested hash value on success or false on failure
      * @throws HashException throws exception if could not hash the data
      */
-    public static function sha384($data, $serialize = false)
+    public static function sha384($data, $serialize = false, $raw_output = false)
     {
-        return self::hash($data, self::SHA384, $serialize);
+        return self::hash($data, self::SHA384, $serialize, $raw_output);
     }
 
 
     /**
      * Make digested hash from a value by this specific hash algorithm
      *
-     * @param  string|mixed  $data       the data is being hashed
-     * @param  bool          $serialize  [optional] if $serialize is true, $data can be any type but resources.
-     *                                   it uses json encode to convert any types to string
+     * @param  string|mixed  $data        the data is being hashed
+     * @param  bool          $serialize   [optional] if $serialize is true, $data can be any type but resources.
+     *                                    it uses json encode to convert any types to string
+     *
+     * @param  bool          $raw_output  [optional] Setting to true will return as raw output data, otherwise the return
+     *                                    value is binhex encoded
      *
      * @return false|string return the digested hash value on success or false on failure
      * @throws HashException throws exception if could not hash the data
      */
-    public static function sha512($data, $serialize = false)
+    public static function sha512($data, $serialize = false, $raw_output = false)
     {
-        return self::hash($data, self::SHA512, $serialize);
+        return self::hash($data, self::SHA512, $serialize, $raw_output);
     }
 }
