13b9be0ea79f90e319472e484ab2958486864990
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-11-13 00:55:37 +0330

    InputListener handler class created

---
+++ PHPGuard/Console/EventHandler/InputListener.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author  Baha2r
+ * @license MIT
+ * Date: 13/Nov/2019 00:54 AM
+ *
+ * Event listener handler class
+ **/
+
+namespace PHPGuard\Console\EventHandler;
+
+
+class InputListener
+{
+
+}
