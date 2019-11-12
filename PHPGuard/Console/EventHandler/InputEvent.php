3bd371a889ad6dd81fdc2ac5c797cd20a43b0d62
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-11-13 00:55:07 +0330

    InputEvent handler class created

---
+++ PHPGuard/Console/EventHandler/InputEvent.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author  Baha2r
+ * @license MIT
+ * Date: 13/Nov/2019 00:53 AM
+ *
+ * Event input handler class
+ **/
+
+namespace PHPGuard\Console\EventHandler;
+
+
+class InputEvent
+{
+
+}
