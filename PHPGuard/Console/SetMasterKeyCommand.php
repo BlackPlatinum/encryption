f06e8ff729708f37782f30d834c203303a7615a4
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-11-14 22:16:06 +0330

    SetMasterKeyCommand class completed

--- PHPGuard/Console/SetMasterKeyCommand.php
+++ PHPGuard/Console/SetMasterKeyCommand.php
@@ -20,9 +20,71 @@ use Symfony\Component\EventDispatcher\EventDispatcher;
 use PHPGuard\Console\EventHandler\InputListener;
 use PHPGuard\Console\EventHandler\InputEvent;
 use PHPGuard\Hash\Hash;
-use PHPScanner\Scanner\Scanner;
 
 class SetMasterKeyCommand extends Command
 {
-    //
+
+    /**
+     * @var string|null Master key
+     */
+    private $master;
+
+    /**
+     * @var Redis Store master key in memory
+     */
+    private $redis;
+
+    /**
+     * @param  string|null  $name  The name of the command. The default name is null, it means it must be set in configure()
+     */
+    public function __construct(string $name = null)
+    {
+        parent::__construct($name);
+        $this->redis = new Redis();
+        $this->redis->connect("127.0.0.1");
+    }
+
+
+    /**
+     * Configures requirements for this command
+     */
+    protected function configure(): void
+    {
+        $this->setName("setAdminKey")
+                ->setDescription("Set admin key for using Guard cryptography system")
+                ->setHelp("<comment>\nSet admin key for using Guard cryptography system. It uses admin key to generate master key due to protect your data.\n</comment>");
+        parent::configure();
+    }
+
+
+    /**
+     * Executes this command
+     *
+     * @param  InputInterface   $input
+     * @param  OutputInterface  $output
+     *
+     * @throws RuntimeException Throws runtime exception if it will be failed to set master key
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
+        $output->writeln("");
+        $output->writeln("<question>>>> Welcome to Guard cryptography system</question><comment>  v2.0.0</comment>");
+        $output->writeln("");
+        $output->writeln("<question>>>> Please enter your admin key:</question>");
+        $output->writeln("");
+        $dispatcher = new EventDispatcher();
+        $listener = new InputListener();
+        $dispatcher->addListener(InputEvent::EVENT_NAME, array($listener, "onInputEvent"));
+        $dispatcher->dispatch(new InputEvent(), InputEvent::EVENT_NAME);
+        $this->master = Hash::sha512("ژ‍‍‍‍‍`گ ث".InputListener::$adminKey."ءو ؛");
+        if (is_null($this->master)) {
+            throw new RuntimeException("[RuntimeException]:\n\nFailed to set master key!\n");
+        }
+        $isInserted = $this->redis->set(Hash::sha256(["This", "Is", "?", "!"], true), $this->master);
+        if (!$isInserted) {
+            throw new RuntimeException("Master key generated successfully, but we can not insert it in memory heap!");
+        }
+        $output->writeln("");
+        $output->writeln("<info>>>> Master key generated successfully!\n</info>");
+    }
 }
