efce64dba1eef1024f878aff16b365d6a503c7ba
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-11-14 22:56:36 +0330

    MasterKey class completed

--- PHPGuard/Core/MasterKey.php
+++ PHPGuard/Core/MasterKey.php
@@ -3,7 +3,7 @@
 /**
  * @author  Baha2r
  * @license MIT
- * Date: 16/Oct/2019 02:39 AM
+ * Date: 14/Nov/2019 22:14 PM
  *
  * Managing master key is done by this class
  **/
@@ -11,7 +11,67 @@
 namespace PHPGuard\Core;
 
 
+use PHPGuard\Exception\MasterKeyException;
+use PHPGuard\Hash\Hash;
+use Redis;
+
 class MasterKey
 {
 
+    /**
+     * @var Redis
+     */
+    private $redis;
+
+    /**
+     * @var false|string
+     */
+    private $mk;
+
+
+    /**
+     * Constructor
+     *
+     * Creates connection with redis database
+     */
+    public function __construct()
+    {
+        $this->redis = new Redis();
+        $this->redis->connect("127.0.0.1");
+        $this->mk = Hash::sha256(["This", "Is", "?", "!"], true);
+    }
+
+
+    /** Returns created master key
+     *
+     * @return string Returns registered master key
+     *
+     * @throws MasterKeyException Throws master key exception if it fails to get master key
+     */
+    public function getMaster()
+    {
+        $masterKey = $this->redis->get($this->mk);
+        if (!$masterKey) {
+            throw new MasterKeyException("Master key has not been set yet!");
+        }
+        return $masterKey;
+    }
+
+
+    /**
+     * Drops registered master key
+     */
+    public function dropMaster(): void
+    {
+        $this->redis->del($this->mk);
+    }
+
+
+    /**
+     * Drops all data in all databases
+     */
+    public function dropAll(): void
+    {
+        $this->redis->flushAll();
+    }
 }
