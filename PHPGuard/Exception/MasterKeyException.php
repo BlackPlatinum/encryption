10b4455a5b4faff31bad0c290d200c8da7f4dac2
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-11-14 22:31:52 +0330

    MasterKeyException class created

---
+++ PHPGuard/Exception/MasterKeyException.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author  Baha2r
+ * @license MIT
+ * Date: 14/Nov/2019 22:31 PM
+ *
+ * Main master key exception class
+ **/
+
+namespace PHPGuard\Exception;
+
+use RuntimeException;
+
+class MasterKeyException extends RuntimeException
+{
+    //
+}
