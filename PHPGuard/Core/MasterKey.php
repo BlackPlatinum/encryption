6575019553eb5f474e8c324a93b4d51d55381cbd
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-11-14 22:18:30 +0330

    MasterKey class created

---
+++ PHPGuard/Core/MasterKey.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author  Baha2r
+ * @license MIT
+ * Date: 16/Oct/2019 02:39 AM
+ *
+ * Managing master key is done by this class
+ **/
+
+namespace PHPGuard\Core;
+
+
+class MasterKey
+{
+
+}
