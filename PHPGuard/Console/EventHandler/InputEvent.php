4a0cce592e2ab3b73bd052596b53ca2c348836be
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-11-14 21:24:22 +0330

    InputEvent handler class completed

--- PHPGuard/Console/EventHandler/InputEvent.php
+++ PHPGuard/Console/EventHandler/InputEvent.php
@@ -11,7 +11,41 @@
 namespace PHPGuard\Console\EventHandler;
 
 
-class InputEvent
+use PHPScanner\Scanner\Scanner;
+use Symfony\Contracts\EventDispatcher\Event;
+
+class InputEvent extends Event
 {
 
+    /**
+     * @var string Event name
+     */
+    public const EVENT_NAME = "input.event";
+
+    /**
+     * @var Scanner Gets input through commandline
+     */
+    private $scn;
+
+
+    /**
+     * Constructor
+     *
+     * Make new instance from Scanner object
+     */
+    public function __construct()
+    {
+        $this->scn = new Scanner();
+    }
+
+
+    /**
+     * Gets entered admin key by admin in commandline
+     *
+     * @return string Return entered admin key
+     */
+    public function getInput()
+    {
+        return $this->scn->next();
+    }
 }
