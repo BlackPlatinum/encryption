94623dcad14a74424984943ce0de69aa94c3554d
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-12-12 20:27:41 +0330

    FreshCommand class created

---
+++ PHPGuard/Console/FreshCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @author  Baha2r
+ * @license MIT
+ * Date: 12/Dec/2019 20:25 PM
+ *
+ * FreshCommand class let you to fresh whole Redis database
+ **/
+
+namespace PHPGuard\Console;
+
+
+use Symfony\Component\Console\Command\Command;
+
+
+class FreshCommand extends Command
+{
+
+}
