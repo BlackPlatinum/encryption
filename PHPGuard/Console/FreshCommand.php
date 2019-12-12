317315f52958739f31d9486de439d59fac1034d9
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-12-12 22:43:58 +0330

    FreshCommand class completed

--- PHPGuard/Console/FreshCommand.php
+++ PHPGuard/Console/FreshCommand.php
@@ -11,10 +11,52 @@
 namespace PHPGuard\Console;
 
 
+use Redis;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\RuntimeException;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 
 class FreshCommand extends Command
 {
 
+    /**
+     * @param  string|null  $name  The name of the command. The default name is null, it means it must be set in configure()
+     */
+    public function __construct(string $name = null)
+    {
+        parent::__construct($name);
+    }
+
+
+    /**
+     * Configures requirements for this command
+     */
+    protected function configure(): void
+    {
+        $this->setName("fresh")
+                ->setDescription("Freshens Redis database")
+                ->setHelp("<comment>\nDrops all data in all databases. Be careful when using this command!\n</comment>");
+        parent::configure();
+    }
+
+
+    /**
+     * Executes this command
+     *
+     * @param  InputInterface   $input
+     * @param  OutputInterface  $output
+     *
+     * @throws RuntimeException Throws runtime exception if it fails to set master key
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
+        $redis = new Redis();
+        $redis->connect("127.0.0.1");
+        $redis->flushAll();
+        $output->writeln("");
+        $output->writeln("<info>>>> Redis database cleaned up!</info>");
+        $output->writeln("");
+    }
 }
