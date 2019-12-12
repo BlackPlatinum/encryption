eb77972d680041586a4e95b2d0016bc70c99b2f6
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-12-12 20:26:11 +0330

    DropMasterKeyCommand class created

---
+++ PHPGuard/Console/DropMasterKeyCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @author  Baha2r
+ * @license MIT
+ * Date: 12/Dec/2019 20:25 PM
+ *
+ * DropMasterKeyCommand class let you to drop registered master key
+ **/
+
+namespace PHPGuard\Console;
+
+
+use Symfony\Component\Console\Command\Command;
+
+
+class DropMasterKeyCommand extends Command
+{
+
+}
