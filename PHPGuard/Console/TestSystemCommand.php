f7ed07caf853254b29194280685863c782707915
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-12-13 00:03:27 +0330

    TestSystemCommand class created

---
+++ PHPGuard/Console/TestSystemCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @author  Baha2r
+ * @license MIT
+ * Date: 12/Dec/2019 00:01 AM
+ *
+ * TestSystemCommand class lets you test guard cryptography system
+ **/
+
+namespace PHPGuard\Console;
+
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\RuntimeException;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TestSystemCommand extends Command
+{
+
+}
