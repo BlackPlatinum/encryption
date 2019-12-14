9926672ba9b78fe9cc851de6b3e40e425fe3c358
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2019-12-15 02:15:17 +0330

    Class name changed

--- PHPGuard/Core/Exceptions/KeyException.php
+++ PHPGuard/Core/Exceptions/KeyException.php
@@ -5,14 +5,14 @@
  * @license MIT
  * Date: 14/Nov/2019 22:31 PM
  *
- * Main master key exception class
+ * Main key exception class
  **/
 
 namespace PHPGuard\Core\Exceptions;
 
 use RuntimeException;
 
-class MasterKeyException extends RuntimeException
+class KeyException extends RuntimeException
 {
     //
 }
