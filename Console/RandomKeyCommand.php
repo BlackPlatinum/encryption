e8bdbf5d50de3cee7dd29ad85e14ff317b4d74f8
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-07-18 01:57:49 +0430

    RandomKeyCommand class created

---
+++ Console/RandomKeyCommand.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @author  BlackPlatinum Developers
+ * @license MIT
+ * Date: 18/Jul/2020 01:55 AM
+ **/
+
+namespace BlackPlatinum\Encryption\Console;
+
+
+class RandomKeyCommand
+{
+
+}
