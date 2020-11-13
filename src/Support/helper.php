78eacdcae718fc246454a25882539e47abc63dd5
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-11-14 02:26:06 +0330

    Unnecessary dir check deleted

--- src/Support/helper.php
+++ src/Support/helper.php
@@ -9,13 +9,6 @@ if (!function_exists('keys_path')) {
      */
     function keys_path()
     {
-        $path = PHP_OS === 'WINNT' ? dirname(__DIR__, 2) . '\keys' . '\\' : dirname(__DIR__, 2) . '/keys/';
-
-        if (!file_exists($path)) {
-            mkdir($path);
-            return $path;
-        }
-
-        return $path;
+        return PHP_OS === 'WINNT' ? dirname(__DIR__, 2) . '\keys' . '\\' : dirname(__DIR__, 2) . '/keys/';
     }
 }
